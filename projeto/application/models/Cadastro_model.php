4fab5af52486af5c6b8aa827b77d8bf5d974346b
luizlobofilho <lp.lobofilho@gmail.com>
2017-04-23 22:14:57 -0300

    Add files via upload

--- projeto/application/models/Cadastro_model.php
+++ projeto/application/models/Cadastro_model.php
@@ -3,8 +3,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cadastro_model extends CI_Model {
 		
-	public function getPessoas()
+	public function cadastraPessoa($nome,$cpf,$email,$senha,$matricula)
 	{
-		return $this->db->get('pessoa')->result();
-	}
+        $cpf = str_replace('-','',$cpf);
+        $cpf = str_replace('.','',$cpf);
+
+
+        $sql = "SELECT * from pessoa WHERE nu_cpf = '$cpf'";
+        // consulta se o cpf já existe no banco
+        $query = $this->db->query($sql);
+
+        if($query->result_array() == null ) {
+            $data =
+                array(
+                    'dsc_nome' => $nome,
+                    'nu_cpf' => $cpf,
+                    'dsc_email' => $email,
+                    'senha' => $senha,
+                    'nu_matricula' => $matricula
+                );
+            // executa o insert na tabela pessoa
+            $this->db->insert('pessoa', $data);
+            return 0;
+        }
+        else{
+            return 1;
+        }
+
+    }
 }
