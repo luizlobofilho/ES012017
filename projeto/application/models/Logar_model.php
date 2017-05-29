b02c8b3bd3be6c1982838dc1840d46f251b202e5
abnergit <denysabner@gmail.com>
2017-05-29 20:08:43 -0300

    Add files via upload

--- projeto/application/models/Logar_model.php
+++ projeto/application/models/Logar_model.php
@@ -3,18 +3,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Logar_model extends CI_Model {
 
+
+    public function logged() {
+        $logged = $this->session->userdata('logged');
+
+        if (!isset($logged) || $logged != true) {
+            #echo 'Voce nao tem permissao para entrar nessa pagina. <a href="/projeto">Efetuar Login</a>';
+            return false;
+		  #die();
+        }
+	   return true;
+	   
+    }
+
     public function validaLogin($cpf,$senha)
     {
         $cpf = str_replace('-','',$cpf);
         $cpf = str_replace('.','',$cpf);
+	   $senha = md5($senha);	   
 
         $sql = "SELECT * FROM pessoa WHERE nu_cpf = '$cpf' AND senha = '$senha'";
         $query = $this->db->query($sql);
-        if($query->result_array() != null ){
-            return $query->result_array();
+	   if($query->result_array() != null || logged()){
+		return $query->result_array();
+		  
         }else{
+		  
             return 0;
         }
 
     }
+
+    
 }
