2ee8a4468b739ce113f367e614c88239c9dc8ff5
luizlobofilho <lp.lobofilho@gmail.com>
2017-06-24 16:21:05 -0300

    Novas implementações nas mensagens
    Novas implementações nas mensagens

--- projeto/application/views/mensagem.php
+++ projeto/application/views/mensagem.php
@@ -8,27 +8,25 @@
         });
 
     });
-
 </script>
 
 <article class="row" id="inicio">
 
     <!--modal mensagem !-->
-    <div class="modal fade" id="modalForum">
+    <div class="modal fade" id="modalMensagem">
         <?php
-        echo form_open('forum/cadastrarForum',array('id'=>'formMensagem','name'=>'formMensagem','role'=>'form') );
+        echo form_open('mensagem/inserirMensagem',array('id'=>'formMensagem','name'=>'formMensagem','role'=>'form') );
         ?>
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Cadastrar Fórum</h4>
+                <h4 class="modal-title" id="myModalLabel">Mensagem</h4>
             </div>
             <div class="modal-body" id="ModalBodyFatness">
                 <div class="form-group">
-                    <?php echo form_label('Nome ','dsc_titulo_forum',array('class'=>'required')); ?>
-                    <?php echo form_input(array("type"=>"text","name"=>"inputTituloForum","id"=>"tituloForum",'class'=>'parametros-group form-control','autofocus'=>'autofocus')); ?>
-                    <?php echo form_label('Descrição ','dsc_forum',array('class'=>'required')); ?>
-                    <?php echo form_input(array("type"=>"text","name"=>"inputDscForum","id"=>"dscForum",'class'=>'parametros-group form-control','autofocus'=>'autofocus')); ?>
+                    <?php echo form_label('Mensagem ','dsc_mensagem',array('class'=>'required')); ?>
+                    <?php echo form_input(array("type"=>"text","name"=>"inputMensagem","id"=>"dscMensagem",'class'=>'parametros-group form-control','autofocus'=>'autofocus')); ?>
+                    <?php echo form_input(array("type"=>"hidden","name"=>"inputForum","id"=>"idForum","value"=>$idForum)); ?>
                 </div>
             </div>
             <div class="modal-footer">
@@ -39,23 +37,29 @@
         <?php echo form_close(); ?>
     </div>
 
-
-
     <div id="container-fluid">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-5 col-sm-offset-1">
                 <div class="well"></br>
                     <button type="button" id="open-modal-mensagem-button" class="btn btn-default">Escrever</button>
-                    <h3>Mensagens</h3>
+                    <?php echo '<h3>'.$tituloForum.'</h3>'; ?>
                     </br>
                     <?php
-                        print_r($mensagens);
+                        if($mensagens == null ) {
+                            echo "Não existe mensagens nesse fórum! Seja o primeiro a publicar :)";
+                        }else{
+                            foreach($mensagens as $mensagem){
+//                                echo '<tr><td><a href="/projeto/index.php/forum/deletarForum?id='.$linha->idn_forum.'"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                                echo '<i>'.$mensagem['dsc_nome'].'</i> diz: '.$mensagem['dsc_mensagem'].'<br/>';
+                            }
+                        }
                      ?>
                     </br></br></br>
                 </div>
             </div>
         </div>
     </div>
+
 </article>
 
 
